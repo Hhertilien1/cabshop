6f3571058c67034d06a6b0c990b36c0d69229d0f
Hhertilien1 <152655709+Hhertilien1@users.noreply.github.com>
2025-02-05 20:21:56 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Admin from './components/Admin';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Admin /> {/* Now this renders the Admin component below the header */}
+
+    </div>
+  );
+}
+
+export default App;
